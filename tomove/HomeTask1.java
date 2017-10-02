9af3b42e6d6aa1e678394d641682728824a5c9ec
DASHA <Katrinka-86@yandex.ru>
2017-10-03 00:18:56 +0300

    no message

---
+++ tomove/HomeTask1.java
@@ -0,0 +1,10 @@
+
+public class HomeTask1 {
+
+	public static void main(String[] args) {
+		String f = "Hello ";
+		String w = "World!";
+		System.out.println(f+w);
+		}
+
+}
---
+++ tomove/HomeTask3.java
@@ -0,0 +1,13 @@
+
+public class HomeTask3 {
+
+	public static void main(String[] args) {
+		int j = 657;
+		int r = j%10;
+		
+		if(r==7){
+			System.out.println("True");}
+			else System.out.println("False");
+	}
+
+}
